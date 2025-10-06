ca9bbf89e54bf7e6a2d81755c2211c90db2a2b61
Kheav-Kienghok <2023500kheav@aupp.edu.kh>
2025-10-06 17:06:33 +0700

    Add CORS to null for development testing

--- internal/server/server.go
+++ internal/server/server.go
@@ -9,6 +9,7 @@ import (
 	"github.com/Kheav-Kienghok/scholarship_portal/internal/middlewares"
 	"github.com/Kheav-Kienghok/scholarship_portal/internal/routes"
 	"github.com/Kheav-Kienghok/scholarship_portal/internal/utils"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -24,8 +25,19 @@ func NewServer(port string, db *database.Database) *Server {
 
 	router := gin.Default()
 	_ = router.SetTrustedProxies(nil)
+
+	// // Add CORS middleware
+	// router.Use(cors.New(cors.Config{
+	// 	AllowOrigins:     []string{"http://localhost:3000", "https://yourdomain.com"},
+	// 	AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	// 	AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+	// 	ExposeHeaders:    []string{"Content-Length"},
+	// 	AllowCredentials: true,
+	// }))
+	router.Use(cors.Default()) // Allows all origins
+
 	router.Use(logging.GinLogger())
-	router.Use(middlewares.RequestLogger()) // Add your custom middleware her
+	router.Use(middlewares.RequestLogger()) // Add your custom middleware here
 
 	// Setup routes
 	routes.SetupRoutes(router, db)
